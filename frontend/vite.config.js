36df2c74dea90e0ea31349c33f73a161b560d226
MeryV <meryvegam16@gmail.com>
2025-01-31 19:04:13 -0600

    Pruebas

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 4000, // Puerto para el servidor de desarrollo
+  },
+  build: {
+    outDir: "dist", // Carpeta de salida para la compilación
+  },
+});
