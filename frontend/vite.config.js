db69cd0536b48a0aee4506d991b8e1ac02a0c2a6
MeryV <meryvegam16@gmail.com>
2025-02-04 13:46:05 -0600

    Pruebas

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 4000, // Puerto para el servidor de desarrollo
+    port: 5000, // Puerto para el servidor de desarrollo
   },
   build: {
     outDir: "dist", // Carpeta de salida para la compilación
